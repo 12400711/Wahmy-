5dbadb3fdbb024fd76a68136b4bdd2e91a85afe3
unknown <mhbsfwt44@gmail.com>
2025-05-05 23:13:54 +0300

    updat

---
+++ src/backendData.js
@@ -0,0 +1,24 @@
+export const ResturantAdress = [
+    {city:'cairo ',adress:' 17 Abdel Khaleq Tharwat - Downtown - Next to Karam El Sham'},
+    {city:'6 October',adress:'Al-Hosary - in front of Al-Hosary Mosque'},
+//    {city:'الساحل الشمالي',adress:'الفودكورت بجوار ب لبن بوابة مارينا "4'},
+    {city:'New cairo',adress:'Ibrahim Street - Al-Korba - next to Balban'},
+//     {city:'فيصل',adress:'الجيزه - شارع فيصل - اول العشرين - امام كرم الشام - امام بلبن'},
+//     {city:'فرع ألف مسكن',adress:'218 معادي السرايات الغربية الف مسكن احمد عرابى'},
+//     {city:'الشيخ زايد',adress:'الشيخ زايد - وصلة دهشور - بجوار محل بلبن'},
+]
+
+
+export const MenuItems = [
+    {itemName:'menu item',itemDescription:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',itemPrice:'9.98',isHighlited:false,imgSrc:'/src/imges/burger-ingriedients-aboutus-section.png',imgAlt:'item-img'},
+    {itemName:'menu item',itemDescription:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',itemPrice:'9.98',isHighlited:false,imgSrc:'/src/imges/burger-ingriedients-aboutus-section.png',imgAlt:'item-img'},
+    {itemName:'menu item',itemDescription:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',itemPrice:'9.98',isHighlited:false,imgSrc:'/src/imges/burger-ingriedients-aboutus-section.png',imgAlt:'item-img'},
+    {itemName:'menu item',itemDescription:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',itemPrice:'9.98',isHighlited:true,imgSrc:'/src/imges/burger-ingriedients-aboutus-section.png',imgAlt:'item-img'},
+    {itemName:'menu item',itemDescription:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',itemPrice:'9.98',isHighlited:false,imgSrc:'/src/imges/burger-ingriedients-aboutus-section.png',imgAlt:'item-img'},
+    {itemName:'menu item',itemDescription:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',itemPrice:'9.98',isHighlited:false,imgSrc:'/src/imges/burger-ingriedients-aboutus-section.png',imgAlt:'item-img'},
+    {itemName:'menu item',itemDescription:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',itemPrice:'9.98',isHighlited:false,imgSrc:'/src/imges/burger-ingriedients-aboutus-section.png',imgAlt:'item-img'},
+    {itemName:'menu item',itemDescription:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',itemPrice:'9.98',isHighlited:true,imgSrc:'/src/imges/burger-ingriedients-aboutus-section.png',imgAlt:'item-img'},
+    {itemName:'menu item',itemDescription:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',itemPrice:'9.98',isHighlited:false,imgSrc:'/src/imges/burger-ingriedients-aboutus-section.png',imgAlt:'item-img'},
+    {itemName:'menu item',itemDescription:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',itemPrice:'9.98',isHighlited:false,imgSrc:'/src/imges/burger-ingriedients-aboutus-section.png',imgAlt:'item-img'},
+    {itemName:'menu item',itemDescription:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',itemPrice:'9.98',isHighlited:false,imgSrc:'/src/imges/burger-ingriedients-aboutus-section.png',imgAlt:'item-img'},
+]
